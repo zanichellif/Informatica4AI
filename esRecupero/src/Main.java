b88a75ab095c529e3fe9f34316db175114ba039b
zanichellif <prof.zanichelli.fabio@darzo.net>
2024-01-10 09:54:07 +0100

    I/O file codice spartano

--- esRecupero/src/Main.java
+++ esRecupero/src/Main.java
@@ -1,7 +1,19 @@
+import java.io.*;
 import java.util.ArrayList;
 
 public class Main {
-    public static void main(String[] args) {
+
+
+
+
+    public static void main(String[] args) throws IOException {
+
+        File f = new File("src" + File.separator + "testo.txt");
+        FileWriter fw = new FileWriter(f);
+        BufferedWriter bw = new BufferedWriter(fw);
+        FileReader fr = new FileReader(f);
+        BufferedReader br = new BufferedReader(fr);
+
         ArrayList<Studenti> elenco = new ArrayList<>();
         Studenti s1 = new Studenti("Mario", "Rossi", 8.0, 9.0);
         StudentiUniversitari s2 = new StudentiUniversitari("Luigi", "Bianchi", 7, 9, "123456");
@@ -10,6 +22,25 @@ public class Main {
         elenco.add(s1);
         elenco.add(s2);
         elenco.add(s3);
+
+        for (Studenti s: elenco) {
+            if (s instanceof StudentiUniversitari)
+                bw.write("universitario");
+            else
+                bw.write("normale");
+            bw.newLine();
+            bw.write(s.getNome());
+            bw.newLine();
+            bw.write(s.getCognome());
+            bw.newLine();
+            bw.write(Double.toString(s.calcola_media()));
+            bw.newLine();
+            if (s instanceof StudentiUniversitari) {
+                bw.write(((StudentiUniversitari) s).getMatricola());
+                bw.newLine();
+            }
+            bw.flush();
+        }
 
         for(int i = 0; i<elenco.size(); i++){
             System.out.println(elenco.get(i).toString());
