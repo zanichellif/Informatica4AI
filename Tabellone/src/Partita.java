313756cf96e47dfdfeef3b92a25bf8caef63406e
zanichelli.fabio <prof.zanichelli.fabio@darzo.net>
2024-01-31 09:59:46 +0100

    Aggiunta schermata tabellone e inizio classe partita

---
+++ Tabellone/src/Partita.java
@@ -0,0 +1,11 @@
+public class Partita {
+
+    private int goal_casa;
+    private int goal_trasferta;
+
+    public Partita(){
+        //TODO import da file
+        goal_casa = 0;
+        goal_trasferta = 0;
+    }
+}
