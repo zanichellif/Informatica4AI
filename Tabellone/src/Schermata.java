409ce330deca4fd17b2743650fd32599a35eb371
zanichelli.fabio <prof.zanichelli.fabio@darzo.net>
2024-01-31 09:02:48 +0100

    Creazione base frame

---
+++ Tabellone/src/Schermata.java
@@ -0,0 +1,19 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowListener;
+
+public class Schermata extends MioFrame implements ActionListener, WindowListener {
+
+    public Schermata() {
+        super();
+    }
+
+    public Schermata(String titolo) {
+        super(titolo);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
