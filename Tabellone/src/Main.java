db0b5f9cc802bf9393379853c5b2995e0f5545e0
zanichelli.fabio <prof.zanichelli.fabio@darzo.net>
2024-02-07 09:40:07 +0100

    Revert "Conclusione esercizio tabellone"
    This reverts commit 7dcf8d0a8899d7ede6719163f23db5f620564c50.

--- Tabellone/src/Main.java
+++ Tabellone/src/Main.java
@@ -1,7 +1,5 @@
-import java.io.IOException;
-
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         Schermata s = new Schermata("Tabellone");
         s.rendiVisibile(s);
     }
